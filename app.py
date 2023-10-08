7cd552685b27a41dd9a8791be8377eefad5efef6
Bhunakit Chantaraseno <bhun@Bhunakits-MacBook-Air.local>
2023-10-08 21:52:12 +0700

    commit

--- app.py
+++ app.py
@@ -16,45 +16,44 @@ def is_valid_youtube_url(url):
     else:
         return False
 
-def generate_summary(url, summary_choice):
-    time.sleep(2)
-    return "Hi"
-    # if not is_valid_youtube_url(url):
-    #     return "Not URL"
+def generate_summary(url, summary_choice, lang):
+    if not is_valid_youtube_url(url):
+        return "Not URL"
 
-    # try:
-    #     video = YouTube(url)
+    try:
+        video = YouTube(url)
         
-    #     supabase.storage.from_('audio').upload("transcript.mpeg", audiofilename)
+        if video.length > 360: 
+            return "Oops! Our AI's attention span is shorter than a sitcom episode. Keep the video under 6 minutes!"
 
-    #     if video.length > 360: 
-    #         return "Oops! Our AI's attention span is shorter than a sitcom episode. Keep the video under 6 minutes!"
+        stream = video.streams.filter(only_audio=True).first()
+        audiofilename = f"/tmp/{video.title}.mpeg"
+        stream.download(filename=audiofilename)
+        openai.api_key = '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'
 
-    #     stream = video.streams.filter(only_audio=True).first()
-    #     audiofilename = f"/tmp/{video.title}.mpeg"
-    #     stream.download(filename=audiofilename)
-    #     openai.api_key = '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'
+        audio_file= open(audiofilename, "rb")
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)["text"]
 
-    #     audio_file= open(audiofilename, "rb")
-    #     transcript = openai.Audio.transcribe("whisper-1", audio_file)["text"]
+        summary_type = ["concise and comprehensive 50 words", "five bullet points (each bullet point no more than 20 words)"]
+        lang_type = {"en": "English", "fr": "French", "es": "Spanish", "th": "Thai", "ja": "Japanese"}
 
-    #     summary_type = ["concise and comprehensive 50 words", "five bullet points"]
+        prompt = f'''Summarize the video transcription delimited by triple quotes in {summary_type[int(summary_choice)]} in {lang_type[lang]}.
+        \n\"\"\"{transcript}\"\"\"\n\n
 
-    #     prompt = f'''Summarize the video transcription delimited by triple quotes in {summary_type[int(summary_choice)]}.
-    #     \n\"\"\"{transcript}\"\"\"
-    #     '''
+        Only output the content
+        '''
 
-    #     response = openai.ChatCompletion.create(
-    #         model="gpt-3.5-turbo",
-    #         messages=[
-    #             {"role": "system", "content": "You are a helpful assistant."},
-    #             {"role": "user", "content": prompt},
-    #         ]
-    #     )
-    #     return f"{video.title}<br>" + response['choices'][0]["message"]["content"]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt},
+            ]
+        )
+        return f"Summarization of {video.title}<br>" + response['choices'][0]["message"]["content"]
     
-    # except Exception as e:
-    #     return f"Error: {str(e)}"
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 
 def cleanup_files(audiofilename):
@@ -70,7 +69,8 @@ def index():
     if request.method == "POST":
         youtube_url = request.form.get("youtube_url")
         summary_choice = request.form.get("summary_choice")
-        summary = generate_summary(youtube_url, summary_choice)
+        translation_language = request.form.get("translation_language")
+        summary = generate_summary(youtube_url, summary_choice, translation_language)
         cleanup_files(f"/tmp/{YouTube(youtube_url).title}.mpeg")
         return jsonify({"summary": summary.strip()})
     return render_template("index.html")
